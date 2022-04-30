c932dfabe941f67c5e3387a564003299b981592a
AlejandroOrejudo2000 <alejandroorejudo2000@gmail.com>
2022-04-30 14:15:43 +0200

    Interacción con la estación de molde

--- Assets/Scripts/Ingame/Interactions/Interactables/MoldStation/MoldCutterInteractable.cs
+++ Assets/Scripts/Ingame/Interactions/Interactables/MoldStation/MoldCutterInteractable.cs
@@ -1,22 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
-public class RayCutterInteractable : GrabInteractable
+public class MoldCutterInteractable : GrabInteractable
 {
     private LineRenderer rayRenderer;
-    private LayerMask raycastMask;
-    [SerializeField] private float maxdistance = 0.4f;
-    [SerializeField] Mold mold;
+    [SerializeField] private Mold mold;
+    [SerializeField] private LayerMask raycastMask;
+    [SerializeField] private float maxdistance = 4f;
 
-    private void Start() {
-        rayRenderer = GetComponent<LineRenderer>();
-        rayRenderer.enabled = false;
-        rayRenderer.useWorldSpace = false;
-        rayRenderer.positionCount = 2;
-        rayRenderer.SetPosition(0, Vector3.down);
-        raycastMask.value = mold.gameObject.layer;
+    private void Start() 
+    {
+        InitBeam();
     }
 
     public override void OnTriggerStart()
@@ -24,33 +21,33 @@ public class RayCutterInteractable : GrabInteractable
         base.OnTriggerStart();
         rayRenderer.enabled = true;
     }
+
     public override void OnTriggerEnd()
     {
         base.OnTriggerEnd();
         rayRenderer.enabled = false;
+        mold.OnCutterStop();
     }
 
     private void Update() {
         if(rayRenderer.enabled)
-        {
-            //Debug.Log("AAAAAAAAA");
-            
+        {           
             if(Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, maxdistance, raycastMask)){
                 rayRenderer.SetPosition(1, transform.TransformPoint(hit.point));
-                Debug.DrawRay(transform.position, -transform.up * hit.distance, Color.red, 1.0f);
-                Debug.Log(hit.transform.gameObject.name);
+                mold.OnCutterHit(hit.point);
             }
             else{
-                rayRenderer.SetPosition(1, Vector3.down);
+                rayRenderer.SetPosition(1, Vector3.down * maxdistance);
             }
         }
     }
 
     private void InitBeam()
     {
+        rayRenderer = GetComponent<LineRenderer>();
         rayRenderer.enabled = false;
         rayRenderer.useWorldSpace = false;
         rayRenderer.positionCount = 2;
-        rayRenderer.SetPosition(0, Vector3.down);
+        rayRenderer.SetPosition(0, Vector3.down * maxdistance);
     }
 }
--- Assets/Scripts/Ingame/Stations/MoldStation.cs
+++ Assets/Scripts/Ingame/Stations/MoldStation.cs
@@ -4,49 +4,8 @@ using UnityEngine;
 
 public class MoldStation : WorkStation
 {
-    [SerializeField] private List<WeaponShape> shapes;
-    [SerializeField] private int currentType;
-    [SerializeField] private int currentShape;
-    [SerializeField] private Mold mold;   
-    [SerializeField] private RayCutterInteractable rayCutter; 
-    [SerializeField] private int shapesPerType = 2;
-    [SerializeField] private ButtonInteractable[] shapeSelectors;
-    private void Start() 
-    {        
-        selectShape(0);
-        
-    }   
-
-    public new void OnEnable() 
-    {
-        base.OnEnable();
-        for(int i = 0; i <= shapeSelectors.Length; i++){
-            shapeSelectors[i].OnPressButton += ()=> swapShape(i);
-        }        
-    }
-
-    public new void OnDisable() 
-    {
-        base.OnDisable();
-        for(int i = 0; i <= shapeSelectors.Length; i++){
-            shapeSelectors[i].OnPressButton -= ()=> swapShape(i);
-        }
-    }
-
-    public void swapShape(int type)
-    {
-        if (currentType == type)
-            currentShape =  (currentShape + 1) % shapesPerType;
-        else{
-            currentType = type;
-            currentShape = 0;
-        }        
-        selectShape(currentType * shapesPerType + currentShape);
-    }
-
-    private void selectShape(int id){
-        mold.renderTemplateShape(shapes[id].points.ToArray());       
-    }
+   
+    [SerializeField] private Mold mold;    
 
     public override bool IsValidResource(Resource resource)
     {
